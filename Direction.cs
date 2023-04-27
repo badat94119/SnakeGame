9e4268164fb93bee7a76c40cef8ec94215712846
badat94119 <datba411@gmail.com>
2023-04-27 14:04:11 +0700

    SnakeGame

---
+++ Direction.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+	public class Direction
+	{
+		public static string DIRECTION_RIGHT = "right";
+		public static string DIRECTION_LEFT = "left";
+		public static string DIRECTION_UP = "up";
+		public static string DIRECTION_DOWN = "down";
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SnakeGame
 {
+
     class Program
     {
-        static int N = 10;
-        static int M = 10;
-        static int speed = 1;
+        static bool isGameOver = false;
+
+        static int N = 15;
+        static int M = 30;
+        static int speed = 500;
         static int score = 0;
 
         static string SKIN = "*";
         static string BRICK = "#";
         static string SPACE = " ";
-       
+        static string APPLE = "@";
+
+        static string direction = Direction.DIRECTION_RIGHT;
+
         static string[,] board = new string[N, M];
         static Snake snake = new Snake();
         static Food food = new Food();
-        
 
+        /// <summary>
+        /// Tính toán vị trí của tường
+        /// </summary>
         private static void calcWall()
         {
-            for (int i = 0; i < N; i++) 
+            for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < M; j++)
                 {
-                    if (i == 0 || i == N - 1 || j == 0 ||j == M-1)
+                    if (i == 0 || i == N - 1 || j == 0 || j == M - 1)
                     {
                         board[i, j] = BRICK;
                     }
@@ -39,11 +49,24 @@ namespace SnakeGame
             {
                 for (int j = 0; j < M; j++)
                 {
-                   int row = snake.Head.Row;
-                   int column = snake.Head.Column; 
-                    if (i ==row && j == column)
+                    //đầu rắn
+                    int row = snake.Head.Row;
+                    int colmn = snake.Head.Column;
+                    if (i == row && j == colmn)
                     {
                         board[i, j] = SKIN;
+                    }
+
+
+                    //thân rắn
+                    List<Point> body = snake.Body;
+                    for (int k = 0; k < body.Count; k++)
+                    {
+                        Point element = body[k];
+                        if (i == element.Row && j == element.Column)
+                        {
+                            board[i, j] = SKIN;
+                        }
                     }
                 }
             }
@@ -51,9 +74,21 @@ namespace SnakeGame
 
         private static void calcFood()
         {
-
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (i == food.Point.Row && j == food.Point.Column)
+                    {
+                        board[i, j] = APPLE;
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// Khởi tạo giá trị ban đầu cho board là các dấu space
+        /// </summary>
         private static void resetBoard()
         {
             for (int i = 0; i < N; i++)
@@ -62,20 +97,24 @@ namespace SnakeGame
                 {
                     board[i, j] = SPACE;
                 }
-
-                Console.WriteLine();
             }
         }
 
+        /// <summary>
+        /// In dữ liệu trong board ra màn hình
+        /// </summary>
         private static void printBoard()
         {
+            Console.WriteLine($"Score: {score}, speed: {speed}");
             for (int i = 0; i < N; i++)
             {
-                for (int j = 0; j < M; j++) 
+                for (int j = 0; j < M; j++)
                 {
                     string value = board[i, j];
-                    if(value.Equals(BRICK))
-                        {
+
+                    //Nếu là wall thì có màu đỏ
+                    if (value.Equals(BRICK))
+                    {
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.BackgroundColor = ConsoleColor.Red;
                         Console.Write(value);
@@ -83,21 +122,110 @@ namespace SnakeGame
                     }
                     else
                     {
-
+                        Console.Write(value);
                     }
-                    Console.Write(value);
                 }
 
                 Console.WriteLine();
             }
         }
+
+        static void ListenKey()
+        {
+            while (!isGameOver)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (direction != Direction.DIRECTION_DOWN)
+                    {
+                        direction = Direction.DIRECTION_UP;
+                    }
+
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (direction != Direction.DIRECTION_UP)
+                    {
+                        direction = Direction.DIRECTION_DOWN;
+                    }
+
+                }
+                else if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    if (direction != Direction.DIRECTION_RIGHT)
+                    {
+                        direction = Direction.DIRECTION_LEFT;
+                    }
+
+                }
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    if (direction != Direction.DIRECTION_LEFT)
+                    {
+
+                        direction = Direction.DIRECTION_RIGHT;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.Clear();
-            resetBoard();
-            calcWall();
-            calcSnake();
-            printBoard();
+            Thread thread = new Thread(Program.ListenKey);
+            thread.Start();
+
+            food.RandomPoint(snake, N, M);
+
+            while (true)
+            {
+                Console.Clear();
+
+                resetBoard();
+
+                //Tính toán vị trí của tường
+                calcWall();
+
+                //Tính toán vị trí của thức ăn
+                calcFood();
+
+                //Tính toán vị trí của rắn
+                calcSnake();
+
+                //In ra dữ liệu đã được tính toán
+                printBoard();
+
+                if (snake.Head.Row == food.Point.Row && snake.Head.Column == food.Point.Column)
+                {
+                    //rắn dài ra
+                    snake.Body.Add(new Point(-1, -1));
+
+                    //điểm và tốc độ tăng lên
+                    score++;
+                    speed -= 50;
+                    speed = speed < 100 ? 100 : speed;
+
+                    //thức ăn đổi vị trí
+                    food.RandomPoint(snake, N, M);
+                }
+
+                //đầu va vào thân thì chết
+                if (snake.IsHeadInBody())
+                {
+                    Console.WriteLine("Game Over");
+                    isGameOver = true;
+                    Console.Clear();
+                    printBoard();
+                    break;
+                }
+
+                snake.move(direction, N, M);
+
+                Task.Delay(speed).Wait();
+            }
+
+            Console.WriteLine("Press any key to exit this game.....");
         }
     }
 }
